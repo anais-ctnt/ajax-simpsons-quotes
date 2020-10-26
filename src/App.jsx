f8501a38ecec3b82cbf0113b3050894f755b7cb0
anais-ctnt <anaiscotinet@gmail.com>
2020-10-26 13:38:52 +0100

    create simpsons quote app

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css';
+import QuoteCard from './components/QuoteCard';
+import axios from 'axios';
+import {Component} from 'react';
+
+const oneSimpsonQuote = [
+  {
+   "quote": "Shoplifting is a victimless crime, like punching someone in the dark.",
+   "character": "Nelson Muntz",
+   "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
+   "characterDirection": "Left"
+  }
+]
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+      this.state = {
+        quote : oneSimpsonQuote[0]
+      };
+    this.getNewQuote = this.getNewQuote.bind(this);
+  }
+
+  getNewQuote(){
+    axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
+    .then(response => response.data)
+    .then(data => {
+      console.log(data);
+      this.setState({
+        quote: data[0],
+      });
+     });
+  }
+
+  render (){
+    return (
+      <div className="App">
+        <QuoteCard quote={this.state.quote}/>
+        <button className="btnNewQuote" type="button" onClick={this.getNewQuote}>GET NEW QUOTE</button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/QuoteCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './QuoteCard.css';
+
+
+function QuoteCard ({quote}){
+  return(
+    <div className="quoteCard">
+       <img className="quoteImage"
+        src={quote.image}
+        alt={quote.character}
+      />
+      <p className="cardCharacterName">{quote.character}</p>
+      <blockquote>"{quote.quote}"</blockquote>
+     
+    </div>
+  );
+}
+
+export default QuoteCard;
